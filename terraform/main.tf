cc48cf429e15e1f46c749606032756bc3984e99a
Winicius Silva <winiciusab12@gmail.com>
2023-12-20 14:12:21 -0300

    add libvirt provider to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,13 @@
 terraform {
-  provider "libvirt" {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.7.6"
+    }
   }
+}
+
+provider "libvirt" {
 
 }
+
