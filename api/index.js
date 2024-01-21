f94c08696b06b6294409d6e01be09d048eef1aea
Winicius Silva <winiciusab12@gmail.com>
2024-01-21 19:22:47 -0300

    add content as required and change mongo default uri

--- api/index.js
+++ api/index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Mongodb setup.
-mongoose.connect(process.env.DB_URL || "mongodb://0.0.0.0:27017");
+mongoose.connect(process.env.DB_URL || "mongodb://localhost:27017");
 
 const MessageModel = mongoose.model('Message', { 
-  content: String
+  content: { type: String, required: true }
 });
 
 // Routes
