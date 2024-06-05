bdb4ecbafca7cbdf3f4b3cde10fdf32a6cb818e0
Winicius Silva <winiciusab12@gmail.com>
2024-06-04 23:48:36 -0300

    add: destroy pools from domain

--- terraform/libvirt/clean.sh
+++ terraform/libvirt/clean.sh
@@ -1,6 +1,11 @@
 VM_NAME=nufuturo
+POOL_NAME=images
+IMAGE_PATH=/home/suporte/storage
 
 virsh destroy $VM_NAME
 virsh undefine $VM_NAME
+virsh pool-destroy $POOL_NAME
+virsh pool-undefine $POOL_NAME
+sudo rm -rf $IMAGE_PATH/*
 terraform destroy -auto-approve
 
